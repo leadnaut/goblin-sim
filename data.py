23bff3345a9aa98f075752eb2e905716766ca5c5
leadnaut <46551563+leadnaut@users.noreply.github.com>
2022-01-03 15:30:15 +1000

    A* Pathfinding added to tools.py
    also a name generator but thats less cool.

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 #World Generation
-TERRAINBOUNDS = {(0, 0.3):"Deep Ocean", 
+TERRAIN_BOUNDS = {(0, 0.3):"Deep Ocean", 
                  (0.3, 0.45): "Medium Ocean",
                  (0.45, 0.5): "Shallow Ocean",
                  (0.5, 0.525): "Beach",
@@ -7,7 +7,7 @@ TERRAINBOUNDS = {(0, 0.3):"Deep Ocean",
                  (0.75, 0.8): "Hills",
                  (0.8, 1.1): "Mountains"}
 
-TERRAINCOLOURS = {"Deep Ocean": (2, 7, 93),
+TERRAIN_COLOURS = {"Deep Ocean": (2, 7, 93),
                   "Medium Ocean": (0, 65, 194),
                   "Shallow Ocean": (173, 216, 230),
                   "Beach": (220, 192, 139),
@@ -15,3 +15,14 @@ TERRAINCOLOURS = {"Deep Ocean": (2, 7, 93),
                   "Hills": (68, 76, 56),
                   "Mountains": (58, 59, 60),
                   "River": (144, 204, 224)}
+
+#Path-finding
+#Represents the relative costs of moving through a terrain type (higher => less likely to pathfind through)
+PATH_COSTS = {"Deep Ocean": 10,
+              "Medium Ocean": 6,
+              "Shallow Ocean": 5,
+              "Beach": 3,
+              "Flatlands": 1,
+              "Hills": 2,
+              "Mountains": 3,
+              "River": 3} 
--- tools.py
+++ tools.py
@@ -1,6 +1,7 @@
 from data import *
 import png
 import random as r
+import heapq
 
 def hmap_to_png(world):
     pnglist = []
@@ -14,16 +15,19 @@ def hmap_to_png(world):
     image = png.from_array(pnglist, "L")
     image.save(f"{world.get_name()}_hmap.png")
 
-
-def tmap_to_png(world):
+def tmap_to_png(world, path):
     pnglist = []
     tmap = world.get_tmap()
-    for row in tmap:
+    for y, row in enumerate(tmap):
         pngrow = []
-        for tval in row:
-            colour = TERRAINCOLOURS.get(tval)
-            pngrow += list(colour)
+        for x, tval in enumerate(row):
+            if (x,y) in path:
+                pngrow += [255, 0, 0]
+            else:
+                colour = TERRAIN_COLOURS.get(tval)
+                pngrow += list(colour)
         pnglist.append(pngrow)
+    
     image = png.from_array(pnglist, "RGB")
     image.save(f"{world.get_name()}_tmap.png")
 
@@ -55,13 +59,62 @@ def world_stats(world):
     for name in biome_tallies.keys():
         print("   ", name +":", biome_tallies.get(name))
 
+
+### PATHFINDING
+class Priority_Queue:
+    def __init__(self):
+        self.elements = [] #List of tuples of form [priority element]
+    
+    def is_empty(self):
+        return not self.elements
+    
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+    
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
+def a_star_pathfinding(world, start, end):
+    frontier = Priority_Queue()
+    frontier.put(start, 0)
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+
+    while not frontier.is_empty():
+        current = frontier.get()
+
+        if current == end:
+            break
+        
+        for next in world.get_adjacents(current):
+            new_cost = cost_so_far[current] + world.get_cost(current, next)
+            #if space not already in path or if a better path into space found
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + world.distance(next, end)
+                frontier.put(next, priority)
+                came_from[next] = current
+    
+    #Rebuild path :)
+    current = end
+    path = []
+    while current != start:
+        path.append(current)
+        current = came_from[current]
+    path.append(start)
+    return path 
+
+
+### OTHER TOOLS
 def generate_name(sound_length):
     vowel_sounds = ["a", "e", "i", "o", "ai", "ee", "oa", "oi", "ow", "ar",
                     "ay", "ou", "ea", "aw", "ir", "ier", "oo", "or", "ur", "er",
                     "'"]
 
-    consonant_sounds = ["b", "br", "c", "cr", "d", "dr", "f", "fr", "g", "gr",
-                        "gn", "h", "j", "k", "l", "m", "n", "p", "pr", "q", 
+    consonant_sounds = ["b", "br", "c", "ck", "cr", "d", "dr", "f", "fr", "g", 
+                        "gr", "gn", "h", "j", "k", "l", "m", "n", "p", "pr", "q", 
                         "qu", "s", "st", "sh", "t", "th", "tr", "v", "x", "z"]
     last = "v"
     name = ""
